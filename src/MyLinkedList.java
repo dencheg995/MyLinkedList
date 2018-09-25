80231eb6af5de4e010b8ec96dba4b1e8891d5416
denisgrebenuk <dencheg995@gmail.com>
2018-09-25 22:45:03 +0300

    change nextNode

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,75 +1,76 @@
 public class MyLinkedList<E> {
-   private Node<E> first;
-   private Node<E> last;
-   private int size = 0;
-    static class Node<E>{
+    private Node<E> first;
+    private Node<E> last;
+    private int size = 0;
+
+    static class Node<E> {
         E element;
         Node<E> next;
-        Node<E> last;
+        Node<E> pred;
 
         public Node(E element, Node<E> next, Node<E> pred) {
             this.element = element;
             this.next = next;
-            this.last = pred;
+            this.pred = pred;
         }
     }
 
-    private void linkFirst(E element){
-        Node <E> f = first;
+    private void linkFirst(E element) {
+        Node<E> f = first;
         Node<E> newNode = new Node<>(element, null, first);
         first = newNode;
-        if(f == null){
-           last=newNode;
-        }
-        else {
+        if (f == null) {
+            last = newNode;
+        } else {
             f.next = newNode;
         }
         size++;
     }
 
-    private void linkLast(E element){
+    private void linkLast(E element) {
         Node<E> l = last;
         Node<E> newNode = new Node<>(element, null, l);
         last = newNode;
-        if(l == null){
+        if (l == null) {
             first = newNode;
-        }
-        else {
+        } else {
             l.next = newNode;
         }
         size++;
     }
 
-    public boolean addFirst(E element){
+    public boolean addFirst(E element) {
         linkFirst(element);
         return true;
     }
 
-    public boolean addLast(E element){
+    public boolean addLast(E element) {
         linkLast(element);
         return true;
     }
 
-    public boolean add(E element){
-      linkLast(element);
-      return true;
+    public boolean add(E element) {
+        linkLast(element);
+        return true;
     }
 
-    public int size(){return size;}
+    public int size() {
+        return size;
+    }
 
-    public E getFirst(){
+    public E getFirst() {
         Node<E> f = first;
         return f.element;
     }
 
-    public E getLast(){
+    public E getLast() {
         Node<E> l = last;
         return l.element;
     }
 
-    public int indexOf(E element){
+    public int indexOf(E element) {
         int index = 0;
-        if(element == null) {
+        if (element == null) {
             for (Node<E> i = first; i != null; i = i.next) {
                 if (i.element == null) {
                     return index;
@@ -77,10 +78,9 @@ public class MyLinkedList<E> {
                 index++;
             }
 
-        }
-        else {
-            for(Node<E> i = first; i != null; i = i.next){
-                if(element.equals(i.element)) {
+        } else {
+            for (Node<E> i = first; i != null; i = i.next) {
+                if (element.equals(i.element)) {
                     return index;
                 }
                 index++;
@@ -90,27 +90,26 @@ public class MyLinkedList<E> {
         return -1;
     }
 
-    public E get(int index){
+    public E get(int index) {
         int ind = 0;
-            for (Node<E> i = first; i != null; i = i.next) {
-                if (ind == index) {
-                    return i.element;
-                }
-                ind++;
+        for (Node<E> i = first; i != null; i = i.next) {
+            if (ind == index) {
+                return i.element;
             }
+            ind++;
+        }
         return null;
     }
 
     public boolean contains(E element) {
         if (element == null) {
-            for(Node<E> i = first; i!=null; i = i.next){
-                if(i.element == null) return true;
+            for (Node<E> i = first; i != null; i = i.next) {
+                if (i.element == null) return true;
                 else return false;
             }
-        }
-        else {
-            for(Node<E> i = first; i != null; i=i.next){
-                if(element.equals(i.element)){
+        } else {
+            for (Node<E> i = first; i != null; i = i.next) {
+                if (element.equals(i.element)) {
                     return true;
                 }
             }
@@ -119,20 +118,20 @@ public class MyLinkedList<E> {
     }
 
     public boolean containsImprove(E element) {
-        return indexOf(element) >=0;
+        return indexOf(element) >= 0;
     }
 
     public void clear() {
-        for(Node<E> i = first; i != null; i = i.next){
-           i.element = null;
+        for (Node<E> i = first; i != null; i = i.next) {
+            i.element = null;
         }
         size = 0;
     }
 
-    public void set(int index, E element){
+    public void set(int index, E element) {
         int ind = 0;
-        for(Node<E> i = first; i != null; i=i.next) {
-            if (ind == index){
+        for (Node<E> i = first; i != null; i = i.next) {
+            if (ind == index) {
                 i.element = element;
             }
             ind++;
@@ -144,47 +143,71 @@ public class MyLinkedList<E> {
         for (Node<E> i = first; i != null; i = i.next) {
             if (ind == index) {
 
-               nextNode(i);
+                nextNode(i);
             }
             ind++;
         }
     }
 
     public void remove(E elements) {
-       if(elements == null)
-        for (Node<E> i = first; i != null; i = i.next) {
-            if (i.element == null) {
-                nextNode(i);
+        if (elements == null)
+            for (Node<E> i = first; i != null; i = i.next) {
+                if (i.element == null) {
+                    nextNode(i);
+                }
+            }
+        else {
+            for (Node<E> i = first; i != null; i = i.next) {
+                if (elements.equals(i.element)) {
+                    nextNode(i);
+                }
             }
         }
-        else {
-           for (Node<E> i = first; i != null; i = i.next) {
-               if (elements.equals(i.element)) {
-                   nextNode(i);
-               }
-           }
-       }
     }
+
+//    E nextNode(Node<E> i) {
+//        E element = i.element;
+//        Node<E> pred = i.last;
+//        Node<E> next = i.next;
+//
+//        if(pred == null){//если предыдущий объект нашего элемента null
+//            first = next;// то первому объекту присваиваем значение следущего элемента после нашего поданного
+//        }
+//        else { //иначе просто предущему элементу нашего объекта присваиваем следущее значение после нашего объекта
+//            pred.next = next;
+//            i.last = null; // а предыдущий узел обнляем
+//        }
+//
+//        if(next == null){ // если следущий элемент после нашего объекта нулл
+//            last = pred;// то последнему объекту присваиваем предыдущий от нашего объекта
+//        }
+//        else { // иначае следущему элементу после нашего объекта присваиваем предыдущее значение нашего объекта
+//            next.last = pred;
+//            i.next = null; // а след элемент после нашего объекта обнуляем
+//        }
+//
+//        i.element = null; // наш элемент обнуляем
+//        size--;// уменьшаем размер листа
+//        return element;
+//    }
+//}
 
     E nextNode(Node<E> i) {
         E element = i.element;
-        Node<E> pred = i.last;
+        Node<E> pred = i.pred;
         Node<E> next = i.next;
 
-        if(pred == null){
-            first = next;
-        }
-        else {
+        if (pred == null) {
+            first = next
+        } else {
             pred.next = next;
-            i.last = null;
-        }
-
-        if(next == null){
-            last = pred;
-        }
-        else {
-            next.last = pred;
             i.next = null;
+        }
+        if (next == null) {
+            last = pred;
+        } else {
+            next.pred = pred;
+            i.pred = null;
         }
 
         i.element = null;
